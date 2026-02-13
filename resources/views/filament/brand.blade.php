df32657c379f1f45f9262f2f6578193c616adfe8
Ricky-zzz <rickgreat1304@gmail.com>
2026-02-13 16:45:28 +0800

    moree rename

--- resources/views/filament/brand.blade.php
+++ resources/views/filament/brand.blade.php
@@ -1,6 +1,6 @@
 <div class="flex items-center gap-x-3">
-    <img src="{{ asset('images/logo.jpg') }}" alt="Golden Gate Academy Logo" class="h-12 rounded-full" style="height: 3rem;">
+    <img src="{{ asset('images/logo.jpg') }}" alt="Golden Gate College Logo" class="h-12 rounded-full" style="height: 3rem;">
     <span class="text-xl font-bold text-indigo-500 dark:text-white">
-        Golden Gate Academy
+        Golden Gate College
     </span>
 </div>
